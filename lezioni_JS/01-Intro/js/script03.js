3c7f8823a62e1276b6e69a5154fade0632b9123e
Luvixs <148796042+Luvixs@users.noreply.github.com>
2025-06-17 09:01:01 +0200

    Add files via upload

--- lezioni_JS/01-Intro/js/script03.js
+++ lezioni_JS/01-Intro/js/script03.js
@@ -2,15 +2,23 @@ let prodotti = ["pasta", "riso", "latte", "uova", "mele", "pollo", "biscotti", "
 let prezzi = [1.00, 0.99, 1.50, 2.00, 1.20, 4.99, 3.50, 2.50, 0.95, 2.50];
 let quants = [4, 1, 2, 1, 3, 2, 5, 1, 5, 1];
 
-let listaSpesa = document.getElementById("listaSpesa");
-let tr = document.createElement("tr");
-let td = document.createElement("td");
-//prodotti.appendChild(td);
 
-prodotti.forEach(prodotti => {
-    if(prodotti){
-        prodotti.innerHTML += `<td>${prodotti}</td>`;
-    }
-    console.log(prodotti);
-    
-})
+let listaSpesa = document.getElementById("listaSpesa");
+let grandTotl = 0;
+
+
+for(let i = 0; i < prodotti.length; i++){
+    let subtotal = quants[i] * prezzi[i];
+    grandTotl += subtotal;
+    listaSpesa.innerHTML += `<tr> 
+                            <td> ${prodotti[i]} </td>
+                            <td> ${prezzi[i]} </td>
+                            <td> ${quants[i]} </td>
+                            <td> ${subtotal.toFixed(2)} </td>
+                            </tr>
+                            `
+}
+
+let totale = document.getElementById("totale");
+totale.innerHTML = grandTotl + " euro";
+
